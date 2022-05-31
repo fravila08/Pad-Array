4e8d159b780152a76ec140c74677983bdd98d78b
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-30 22:47:44 -0700

    COMPLETE

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,11 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    if min_size == 0 or min_size == len(array):
+        return array
+    else:
+        itirate= min_size - len(array)
+        while itirate != 0:
+            array.append(value)
+            itirate= itirate-1
+    return array
+    
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,7 @@
 # Write unit tests!
+from pad_array import pad
+
+print(pad([1,2,3], 6)== [1, 2, 3, None, None, None])
+print(pad([1,2,3], 5, "it's working")== [1, 2, 3, "it's working", "it's working"])
+print(pad([1,2,3], 0,'Nope')== [1,2,3])
+print(pad([1,2,3], 3,'Nope')== [1,2,3])
